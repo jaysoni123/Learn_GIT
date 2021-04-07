691e662f1a45dee3a45da6b50d1e41bcc0a98f21
Dhananjay Choksi <soni.jay008@gmail.com>
2021-04-08 01:11:11 +0530

    Hello World first- Commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
-
+// hello world
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
